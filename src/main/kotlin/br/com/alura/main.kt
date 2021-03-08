121a4db7fb9a60f9f47ca78d53aed10a5ef4e393
Marcelo Felix <celofelix93@gmail.com>
2021-03-08 15:04:34 -0300

    Testando comportamentos de escrita como adição e atualização de valores com os método put(), putIfAbsent(). Também testado formas de remoção com o método remove()

--- src/main/kotlin/br/com/alura/main.kt
+++ src/main/kotlin/br/com/alura/main.kt
@@ -1,5 +1,3 @@
-@file:Suppress("NAME_SHADOWING")
-
 package br.com.alura
 
 fun main() {
@@ -12,11 +10,15 @@ fun main() {
     Existe a forma de operador infix para diminuir a quantia de código.
     O operador é o to. Ele deve estar entre a chave e o valor
      */
-    val pedidos: Map<Int, Double> = mapOf<Int, Double>(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
+    val pedidos: MutableMap<Int, Double> = mutableMapOf<Int, Double>(
+            Pair(1, 20.0),
+            Pair(2, 34.0),
+            3 to 50.0)
+
     println(pedidos)
 
     /*
-    Ao fazer o acesso como o de leitura de um valor através da chave deve-se tomar alguns cuidados
+    Os compotamentos como o de leitura de um valor através da chave deve-se tomar alguns cuidados
     Por exemplo, se tentar acessar uma chave que não existe, vai ser devolvido um null
     Por isso é importante fazer uma safe call, assim evitamos retorno de valores nulos
      */
@@ -25,8 +27,40 @@ fun main() {
         println("pedido $it")
     }
 
-   for(pedido in pedidos) {
-       println("Número do pedido: ${pedido.key}" + " Valor do pedido: ${pedido.value}")
-   }
+    for (p in pedidos) {
+        println("Código pedido: ${p.key}" + " Valor do pedido: ${p.value}")
+    }
+
+    /*
+    Comportamento de escrita como o de inserção pode ser feito informar uma chave dentro de colchetes
+    A outra forma para adicionar um novo valor ao Map é através do método put()
+    Com o método put() basta informar dentro do parenteses a nova chave e o valor
+    Além de adicionar essas formas servem para atualizar um registro existente dentro do Map
+    Se a chave informada já exista ele irá atualizar ao invés de inserir um novo registro.
+    Caso queira adicionar um valor somente se a chave não existir você pode usar o método putIfAbsent()
+    Com o método utIfAbsent() o valor só será adicionado se a chave não existir
+     */
+
+    pedidos[4] = 70.0
+    println(pedidos)
+    pedidos.put(5,50.0)
+    println(pedidos)
+    pedidos[1] = 100.0
+    pedidos.putIfAbsent(6, 200.0)
+    println(pedidos)
+    pedidos.putIfAbsent(6, 200.0)
+    println()
+
+    /*
+    Também é possível remover um valor de um Map através da sua chave
+    Existe o método remove() que basta informar a chave e assim ele irá deletar
+    O método remove também existe a sobrecarga para passar a chave e o valor
+    A remoção só será feita caso a chave e o valor estejam corretos, caso contrario não será removido
+     */
+    pedidos.remove(6)
+    println(pedidos)
+    pedidos.remove(3, 100.0)
+
+
 }
 
