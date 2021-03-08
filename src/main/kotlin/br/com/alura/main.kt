555d0604d483b004e9b6e62dd2c97170e507df6b
Marcelo Felix <celofelix93@gmail.com>
2021-03-08 10:42:38 -0300

    Criado testes de criação para uma collection não mutável e criado uma classe simples para teste de um membro com tipo de lista mutável com acesso privado e um segundo membro como uma collection não mutável para ser feito acesso aos membros princiais através desse membro não mutável, no caso a lista mutável, dessa forma tornando os membros seguros

--- src/main/kotlin/br/com/alura/main.kt
+++ src/main/kotlin/br/com/alura/main.kt
@@ -2,6 +2,38 @@ package br.com.alura
 
 fun main() {
 
-    
+    val banco = BancoDeNomes()
+//    banco.nomes.sal("Alex")
+    banco.salva("Alex")
+    println(banco.nomes)
+    println(BancoDeNomes().nomes)
+}
 
+class BancoDeNomes {
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+
+    val nomes: Collection<String> get() = Companion.dados
+
+    fun salva(nome: String) {
+        Companion.dados.add(nome)
+    }
+}
+
+fun testaCollection() {
+    val nomes: Collection<String> = mutableListOf("Alex", "Fran", "Gui", "Maria", "Ana")
+
+    for (nome: String in nomes) {
+        println(nomes)
+    }
+
+//    nomes.add("Marcelo")
+    println(nomes)
+    println(("Contém o nome? ${nomes.contains("Marcelo")}"))
+    println("O tamanho da lista é: ${nomes.size}")
+//    nomes.remove("Marcelo")
+    println(nomes)
+    println("O tamanho da lista é: ${nomes.size}")
 }
